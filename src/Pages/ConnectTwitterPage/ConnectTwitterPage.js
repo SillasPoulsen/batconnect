66371ea396585201a8fc4a60762ae0677b639c18
Sillas Victor <Sillas_2@hotmail.com>
2022-03-19 15:27:30 +0000

    add routing

--- src/Pages/ConnectTwitterPage/ConnectTwitterPage.js
+++ src/Pages/ConnectTwitterPage/ConnectTwitterPage.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 export default function Landingpage() {
+  const [profile, setProfile] = useState({
+    image:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    Name: "Name",
+    Bio: "",
+  });
+
   return (
     <div>
       <img
         className="profile-picture"
-        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        src={profile.image}
         alt="profilepicture"
       />
-      <h3>Name</h3>
-      <p>Bio:</p>
+      <h3>{profile.Name}</h3>
+      <p>Bio:{profile.bio}</p>
       <a className="signup-btn" href="google.com">
         Mint NFT
       </a>
