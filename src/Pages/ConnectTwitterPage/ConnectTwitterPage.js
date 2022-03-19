f48c4106e814f17b2cdbd98ac0485c977ed6b664
Sillas Victor <Sillas_2@hotmail.com>
2022-03-19 15:10:21 +0000

    initialize the repo, and add landing page

---
+++ src/Pages/ConnectTwitterPage/ConnectTwitterPage.js
@@ -0,0 +1,16 @@
+export default function Landingpage() {
+  return (
+    <div>
+      <img
+        className="profile-picture"
+        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        alt="profilepicture"
+      />
+      <h3>Name</h3>
+      <p>Bio:</p>
+      <a className="signup-btn" href="google.com">
+        Mint NFT
+      </a>
+    </div>
+  );
+}
---
+++ src/Pages/LandingPage/Components/MainContent.js
@@ -0,0 +1,16 @@
+export default function Maincontent() {
+  return (
+    <div className="main">
+      <div className="leftside-content">
+        <h1>Bring your followers to web3</h1>
+        <p>Own your content, followers and income streams. </p>
+        <a className="signup-btn" href="google.com">
+          Connect twitter
+        </a>
+      </div>
+      <div className="rightside-content">
+        <img src="https://i.ibb.co/KKp2TS1/Group-17-1.png" alt="illustration" />
+      </div>
+    </div>
+  );
+}
