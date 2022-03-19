a29ca4355df05c9de0c3d279b68037c3cb6ab462
Sillas Victor <Sillas_2@hotmail.com>
2022-03-19 15:19:00 +0000

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import ConnectTwitterPage from "./Pages/ConnectTwitterPage/ConnectTwitterPage";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
-      <ConnectTwitterPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/connect" element={<ConnectTwitterPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
