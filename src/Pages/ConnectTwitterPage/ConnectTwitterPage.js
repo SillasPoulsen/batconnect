243bd5d3e9e7f51cdd951e3fe179f761877ef0ad
FracaShaw <fracashaw@gmail.com>
2022-03-19 18:37:50 +0000

    started manipulating auth0 json

--- src/Pages/ConnectTwitterPage/ConnectTwitterPage.js
+++ src/Pages/ConnectTwitterPage/ConnectTwitterPage.js
@@ -1,6 +1,10 @@
 import { useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 export default function Landingpage() {
+  //const { user } = useAuth0();
+  //const { name, picture, email } = user;
   const [profile, setProfile] = useState({
     image:
       "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
