3a1c9228b5aaa7e2098174f260e3b749215fb3f4
FracaShaw <fracashaw@gmail.com>
2022-03-20 12:43:31 +0000

    minor udpate

--- src/Pages/LandingPage/Components/twitter_api_call.js
+++ src/Pages/LandingPage/Components/twitter_api_call.js
@@ -6,11 +6,12 @@ var T = new Twit({
   consumer_secret: 'RJGPjzpU5CXgUUgu0EY9iIK9Cmpd5rLqQ5H8zHDgluKyrvSNxV',
   app_only_auth:        true
 });
-T.get('search/tweets', { q: 'banana since:2011-07-11', count: 1 }, function(err, data, response) {
-  console.log(data)
-})
-T.get('user/show/', { screen_name: 'fracashawg' }, function (err, data, response) {
+// T.get('search/tweets', { q: 'banana since:2011-07-11', count: 1 }, function(err, data, response) {
+//   console.log(data)
+// })
+T.get('followers/ids', { screen_name: 'FracaShawG' }, function (err, data, response) {
 console.log(data);
+//console.log(response);
 console.log(err);
 //console.log(response);
 })
