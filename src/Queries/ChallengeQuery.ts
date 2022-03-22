a8dc835266fb2f8f26b5957d91a11341dc82278d
FracaShaw <fracashaw@gmail.com>
2022-03-22 17:08:51 +0000

    nuked it for franz version

--- src/Queries/ChallengeQuery.ts
+++
@@ -1,5 +0,0 @@
-export const challengeQuery = `
-    query($request: ChallengeRequest!) {
-     challenge(request: $request) { text }
-    }
-`
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import Footer from "./footer";
-import Loading from "./loading";
-import NavBar from "./nav-bar";
-
-export { Footer, Loading, NavBar };
