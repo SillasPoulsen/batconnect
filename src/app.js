ca40a967d66820ff00161d6e33da940c99dc1cf1
João Pedro Correia de Oliveira <jocorrei42miles@milesinthesky.local>
2022-03-24 20:18:12 +0000

    still making some changes

--- src/app.js
+++ src/app.js
@@ -3,14 +3,11 @@ import { Route, Switch } from "react-router-dom";
 import { NavBar, Footer } from "./components";
 import { Home, LensProfile, Profile, WalletProfile } from "./pages";
 
-import "./app.css";
-
 const App = () => {
 
   return (
-    <div id="app" className="d-flex flex-column h-100">
+    <div id="app">
       <NavBar />
-      <div className="container flex-grow-1">
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/profile" component={Profile} />
@@ -23,7 +20,6 @@ const App = () => {
             exact component={LensProfile}
           />
         </Switch>
-      </div>
       <Footer />
     </div>
   );
