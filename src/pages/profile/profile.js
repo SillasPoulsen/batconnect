1cbac6b92de54e681d28701dd2a471e781a05666
João Pedro Correia de Oliveira <mgjoao41@gmail.com>
2022-03-24 22:42:14 +0000

    making myself a contributor

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,9 +1,6 @@
 import React from "react";
-
 import { useState, useEffect } from "react";
-
 import { ethers } from "ethers";
-
 import lensHubABI from "../../abi/lensHubABI.json";
 
 import { useAuth0 } from "@auth0/auth0-react";
@@ -31,10 +28,6 @@ const Profile = () => {
       const account = accounts[0];
       console.log("Found an authorized account: PS(WE ARE RUNNING THIS TWICE) ", account);
       setCurrentAccount(account);
-
-      // Setup listener! This is for the case where a user comes to our site
-      // and ALREADY had their wallet connected + authorized.
-      //setupEventListener();
     } else {
       console.log("No authorized account found");
     }
@@ -56,48 +49,10 @@ const Profile = () => {
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
 
-      // Setup listener! This is for the case where a user comes to our site
-      // and connected their wallet for the first time.
-      //setupEventListener();
     } catch (error) {
       console.log(error);
     }
   };
-
-  // Setup our listener.
-  // const setupEventListener = async () => {
-  //   // Most of this looks the same as our function askContractToMintNft
-  //   try {
-  //     const { ethereum } = window;
-
-  //     if (ethereum) {
-  //       // Same stuff again
-  //       const provider = new ethers.providers.Web3Provider(ethereum);
-  //       const signer = provider.getSigner();
-  //       const connectedContract = new ethers.Contract(
-  //         CONTRACT_ADDRESS,
-  //         lensHubABI.abi,
-  //         signer
-  //       );
-
-  //       // THIS IS THE MAGIC SAUCE.
-  //       // This will essentially "capture" our event when our contract throws it.
-  //       // If you're familiar with webhooks, it's very similar to that!
-  //       connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
-  //         console.log(from, tokenId.toNumber());
-  //         alert(
-  //           `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
-  //         );
-  //       });
-
-  //       console.log("Setup event listener!");
-  //     } else {
-  //       console.log("Ethereum object doesn't exist!");
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
 
   const askContractToCreateProfile = async () => {
     try {
