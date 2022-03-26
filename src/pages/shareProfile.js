e2508d98f4feba8f2a091037cde32d5bbbe46dfe
FracaShaw <fracashaw@gmail.com>
2022-03-26 16:46:29 +0000

    nmbr of followers

--- src/pages/shareProfile.js
+++ src/pages/shareProfile.js
@@ -24,6 +24,10 @@ function ShareProfile() {
           >
             Copy link
           </button>
+          <a class="twitter-share-button"
+          href="https://twitter.com/intent/tweet?text=Hello%20world"
+          data-size="large">
+          Tweet</a>
         </form>
       </div>
     </>
